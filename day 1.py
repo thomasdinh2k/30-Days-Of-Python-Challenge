f54bc280ef0c773bd8df578912e165ea9d7ca202
thomasdinh2k <dnstung@icloud.com>
2023-05-10 12:38:19 +0700

    first commit on new branch

--- day 1.py
+++ day 1.py
@@ -1,2 +1,23 @@
-print("Hello World!")
-print("Testing Commit")
+# Find an Euclidian distance between (2, 3) and (10, 8)
+
+
+# Calculate the Square root
+def sqr(x):
+    a = 1
+    compe = 0
+    while a < 1000:
+        compe = a * a
+        if x - compe == 0:
+            return a
+        else:
+            a += 1
+    return f"Cannot find the square root of {x} "
+
+def distance(p,q):
+    calc = (p-q) ** 2
+    return sqr(calc)
+
+print(distance(2,3))
+print(distance(10,8))
+
+print(sqr(89))
