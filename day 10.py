444693f5277fe108adb15539a9b4a0ed29589b34
thomasdinh2k <work.dnstung@gmail.com>
2023-05-18 18:20:24 +0700

    Completed Day 10

--- day 10.py
+++ day 10.py
@@ -1,5 +1,6 @@
 from InteractiveData.country import countries
 
+
 def iterate(start_num, end_num, direction):
     if direction == 'forward':
         print(f"Iterating from {start_num} to {end_num}")
@@ -28,7 +29,7 @@ def iterate(start_num, end_num, direction):
 
 def make_triangle(calls):
     i = 0
-    while i <= calls+1:
+    while i <= calls + 1:
         if i == 0:
             i += 1
         else:
@@ -37,22 +38,22 @@ def make_triangle(calls):
 
 
 def make_sqr(length):
- step = 0
- if length <= 0:
-     print(f"Can\'t make a shape out of given length {length}")
- else:
-     for i in range(length):
-         print("# " * length)
+    step = 0
+    if length <= 0:
+        print(f"Can\'t make a shape out of given length {length}")
+    else:
+        for i in range(length):
+            print("# " * length)
 
 
 def multiplication_table(number):
-    for multiplier in range(10+1):
+    for multiplier in range(10 + 1):
         result = number * multiplier
         print(f"{number} x {multiplier} = {result}")
 
 
 def lang_items():
-    lang = ['Python', 'Numpy','Pandas','Django', 'Flask']
+    lang = ['Python', 'Numpy', 'Pandas', 'Django', 'Flask']
     [print(item) for item in lang]
     [print(item) for item in lang if item.startswith("F")]
 
@@ -72,19 +73,19 @@ def sum_all(number, type=""):
         print(total)
     elif type == 'Odd':
         for i in range(number + 1):
-            if i % 2 != 0:      # Odd-number
+            if i % 2 != 0:  # Odd-number
                 total += i
         print(total)
     elif type == 'Even':
         for i in range(number + 1):
-            if i % 2 == 0:      # Even-number
+            if i % 2 == 0:  # Even-number
                 total += i
         print(total)
     else:
         total_even = 0
         total_odd = 0
         for i in range(number + 1):
-            if i % 2 != 0:      # Odd-number
+            if i % 2 != 0:  # Odd-number
                 total_odd += i
             if i % 2 == 0:  # Even-number
                 total_even += i
@@ -97,13 +98,46 @@ def print_country():
 
 def reverse_order(lst):
     reversed_lst = []
-    for i in range(len(lst)-1, -1, -1):
+    for i in range(len(lst) - 1, -1, -1):
         print(lst[i])
         reversed_lst.append(lst[i])
     print(reversed_lst)
 
-from InteractiveData.
-def country_facts():
-    print(country_fact)
 
-country_facts()
+from InteractiveData.CountriesData import country_fact
+
+
+def country_facts():
+    language_set = set()
+    for item in country_fact:
+        # print(item['languages'])
+        language_set.update(item['languages'])
+    sorted_language_set = sorted(language_set)
+    print(f"There are {len(country_fact)} countries")
+    print(f"There are {len(language_set)} languages"
+          f" spoken all over the world, which are: {', '.join(sorted_language_set)}")
+    # print(language_set)
+    lang_count = {}
+    # Find top 10 most spoken language
+    for item in language_set:
+        lang_count[item] = 0
+    for item in country_fact:
+        for item in item["languages"]:
+            lang_count[item] += 1  # Properly count the language in the dataset
+    sorted_lang_count = sorted(list(lang_count.items()), key=lambda x: x[1], reverse=True)      # Sort a dictionary by its value
+    top_10 = sorted_lang_count[:10]
+    output_str = ""
+    for i, (key, value) in enumerate(top_10):       # Make a list of top 10 languages
+        output_str += f"{i + 1}. {key}: {value}\n"
+    print(f"Top 10 spoken languages:\n{output_str}")
+    # Top 10 populated country
+    country_population = {}
+    for item in country_fact:
+        country_population[item["name"]] = item["population"]
+
+    max_item = max(country_population.items(), key=lambda x:x[1])
+    print(f"The most populated country is {max_item[0]} which population is {max_item[1]}")
+
+
+
+print("Day 10 Completed!")
